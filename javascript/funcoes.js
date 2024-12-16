0f9424e5b61eed507ee5221e97af767c670351e3
Andre-ctr <prof.andresos@gmail.com>
2024-12-15 22:00:06 -0300

    aula

---
+++ javascript/funcoes.js
@@ -0,0 +1,12 @@
+// JavaScript Document
+
+
+	function acendeLampada(){
+		
+		document.getElementById("luz").src="image/lampada02.jpg";
+	}
+
+	function apagaLampada(){
+		
+		document.getElementById("luz").src="image/lampada01.jpg";
+	}
